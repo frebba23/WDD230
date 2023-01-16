0b26d9563c80f7a8fe416df97471ae0aa143a8bb
frebba23 <frebba2@gmail.com>
2023-01-16 21:53:05 +0200

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 let year = new Date().getFullYear();
-
 let date = `&copy; ${year}.:|:. Freddy Chivhenge .:|:. Zimbabwe`;
-
 document.getElementById(`date`).innerHTML = date;
+
+let modify = new Date(document.lastModified);
+document.getElementById('modified').innerHTML = modify;
